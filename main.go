2a78ae5599eb8433b7e0d70744a19f74c5113728
tinyhui.wang <tinyhui.wang@gmail.com>
2021-07-17 23:18:04 +0800

    删除指定目录下的target，用于清理磁盘空间

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	filePath := flag.String("f",
+		"/Users/tinyhuiwang/workspaces/bjca.cn.ws/api-gateway-manager",
+		"目录")
+
+	realRmove := flag.Bool("rr",
+		false,
+		"是否真的要删除")
+	flag.Parse()
+	fmt.Println("----- file list start-----")
+	files, _ := ioutil.ReadDir(*filePath)
+	for _, f := range files {
+		delTarget(f, *filePath, *realRmove)
+	}
+	fmt.Println("----- file list end-----")
+}
+
+func delTarget(current fs.FileInfo, parent string, rr bool) {
+	PthSep := string(os.PathSeparator)
+	currentAbs := parent + PthSep + current.Name()
+	if current.Name() == "pom.xml" && !current.IsDir() {
+		delTarget := parent + PthSep + "target"
+		if Exists(delTarget) {
+			fmt.Println("需要删除 target目录", delTarget)
+			//os.RemoveAll(delTarget)
+		}
+		return
+	}
+	if current.IsDir() && current.Name() != "target" {
+		files, _ := ioutil.ReadDir(currentAbs)
+		for _, f := range files {
+			delTarget(f, currentAbs, rr)
+		}
+	}
+}
+
+func Exists(path string) bool {
+	_, err := os.Stat(path) //os.Stat获取文件信息
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		return false
+	}
+	return true
+}
